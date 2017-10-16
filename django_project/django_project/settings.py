9a9d2d5b2528807a92526c8dbe67037187b4777b
Wasim Akram Khan <wasimblogs4@hotmail.com>
2017-10-16 12:27:23 +0545

    Edited sensitive information

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -107,9 +107,9 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dummypaaila@gmail.com'
+EMAIL_HOST_USER = '*****paaila@gmail.com'
 
-EMAIL_HOST_PASSWORD = 'dummypaaila4488'
+EMAIL_HOST_PASSWORD = '************'
 
 
 
